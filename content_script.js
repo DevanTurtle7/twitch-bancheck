abbf1b1b6d9ece2b2fb5fed2e0a42eda1fbcbe62
Devan Kavalchek <devankav7@gmail.com>
2021-07-13 13:32:17 -0400

    refactored content script to use jquery

--- content_script.js
+++ content_script.js
@@ -1,19 +1,19 @@
 var prevName;
 
-document.addEventListener('mousemove', function (e) {
-    let target = e.target;
 
-    if (target.classList.contains("chat-author__display-name")) {
-        let currentName = target.dataset.aUser;
 
-        if (currentName != prevName) {
-            console.log(currentName);
+$(document).ready(function () {
+    $(document).mousemove(function (e) {
+        let target = e.target;
+
+        if (target.classList.contains("chat-author__display-name")) {
+            let currentName = target.dataset.aUser;
+
+            if (currentName != prevName) {
+                console.log(currentName);
+            }
+
+            prevName = currentName;
         }
-
-        prevName = currentName;
-    }
-}, false);
-
-$(document).ready(function() {
-    console.log("testing jquery!");
+    });
 });
