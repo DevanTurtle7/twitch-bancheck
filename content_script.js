d21a417a377b0f5600f6c7d35c425c1cf5b2204c
Devan Kavalchek <devankav7@gmail.com>
2021-07-13 13:42:28 -0400

    stubbed api call and added twitch credentials

--- content_script.js
+++ content_script.js
@@ -1,16 +1,28 @@
 var prevName;
-
-
+var broadcasterIds = ["198704263"];
 
 $(document).ready(function () {
     $(document).mousemove(function (e) {
         let target = e.target;
 
-        if (target.classList.contains("chat-author__display-name")) {
+        if (target.classList.contains('chat-author__display-name')) {
             let currentName = target.dataset.aUser;
 
             if (currentName != prevName) {
                 console.log(currentName);
+
+                // Todo: Specify the user id in the call so we don't have to sort through all banned users
+                $.ajax({
+                    url: 'https://api.twitch.tv/helix/moderation/banned?broadcaster_id=' + broadcasterIds[0],
+                    type: 'GET',
+                    headers: {
+                        'Authorization': 'Bearer ' + accessToken,
+                        'Client-Id': + clientId
+                    },
+                    data: {
+
+                    }
+                })
             }
 
             prevName = currentName;
