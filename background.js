a4aee429a90be4a011b60f8c3b021da29c3befee
Devan Kavalchek <devankav7@gmail.com>
2021-07-13 01:36:30 -0400

    updated page action

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/*
 chrome.runtime.onInstalled.addListener(function () {
 	chrome.storage.sync.set({ color: '#3aa757' }, function () {
 		console.log('The color is green.');
@@ -6,10 +7,11 @@ chrome.runtime.onInstalled.addListener(function () {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: { hostEquals: 'twitch.tv' },
+				pageUrl: { hostContains: 'twitch.tv' },
 			})
 			],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
 	});
 });
+*/
