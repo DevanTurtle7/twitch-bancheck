89886ecf17c708c38d0ed0bfb7d1f3c488c985fb
Devan Kavalchek <devankav7@gmail.com>
2021-07-14 02:03:11 -0400

    updated output

--- redirect_site/main.js
+++ redirect_site/main.js
@@ -1,6 +1,2 @@
 $(document).ready(function () {
-    var pattern = /(?<=access_token=).+?(?=&)/;
-    var accessToken = window.location.href.match(pattern)[0];
-
-    console.log(accessToken);
 })
--- redirect_site/token_grabber.js
+++ redirect_site/token_grabber.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     var pattern = /(?<=access_token=).+?(?=&)/;
     var accessToken = window.location.href.match(pattern)[0];
 
-    console.log(accessToken);
-
-    chrome.storage.sync.set({ access_token: 'accessToken' }, function () {
-        console.log('updated access token');
-    });;
+    if (accessToken != null) {
+        chrome.storage.sync.set({ access_token: accessToken }, function () {
+            console.log('updated access token');
+        });
+    } else {
+        console.log("Something went wrong");
+    }
 })
