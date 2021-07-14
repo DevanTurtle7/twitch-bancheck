b71bc7b163150e95423079964eef55a849a63042
Devan Kavalchek <devankav7@gmail.com>
2021-07-14 03:12:18 -0400

    updated storage call and content script

--- content_script.js
+++ content_script.js
@@ -1,6 +1,5 @@
 var prevName;
 var broadcasterIds = ["198704263"];
-var accessToken;
 
 $(document).ready(function () {
     $(document).mousemove(function (e) {
@@ -12,15 +11,23 @@ $(document).ready(function () {
             if (currentName != prevName) {
                 console.log(currentName);
 
-                // Todo: Specify the user id in the call so we don't have to sort through all banned users
-                $.ajax({
-                    url: 'https://api.twitch.tv/helix/moderation/banned?broadcaster_id=' + broadcasterIds[0],
-                    type: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + accessToken,
-                        'Client-Id': + clientId
-                    },
-                })
+                chrome.storage.sync.get("access_token", function (obj) {
+                    console.log(obj.access_token)
+
+                    // Todo: Specify the user id in the call so we don't have to sort through all banned users
+                    $.ajax({
+                        url: 'https://api.twitch.tv/helix/moderation/banned?broadcaster_id=' + broadcasterIds[0],
+                        type: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + obj.access_token,
+                            'Client-Id': clientId
+                        }, success: function(data) {
+                            console.log(data);
+                        }, error: function(data) {
+                            console.log(data);
+                        }
+                    })
+                });
             }
 
             prevName = currentName;
--- redirect_site/token_grabber.js
+++ redirect_site/token_grabber.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     var accessToken = window.location.href.match(pattern)[0];
 
     if (accessToken != null) {
-
-        
+        chrome.storage.sync.set({ access_token: accessToken }, function () {
+        }); 
     } else {
         console.log("Something went wrong");
     }
