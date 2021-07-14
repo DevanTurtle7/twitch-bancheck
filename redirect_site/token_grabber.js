b53f3f2d41dc2fc5ef043a8486b2acfb4aacb881
Devan Kavalchek <devankav7@gmail.com>
2021-07-14 02:20:38 -0400

    updated token grabber to get refresh token

--- redirect_site/token_grabber.js
+++ redirect_site/token_grabber.js
@@ -3,6 +3,19 @@ $(document).ready(function () {
     var accessToken = window.location.href.match(pattern)[0];
 
     if (accessToken != null) {
+        $.ajax({
+            url: 'https://id.twitch.tv/oauth2/token?client_id=' + clientId +
+                '&client_secret=' + clientSecret +
+                '&code=' + accessToken +
+                '&grant_type=authorization_code&redirect_uri=' + redirectUri,
+            type: 'POST',
+            success: function(data) {
+                console.log(data);
+            }, error: function(data) {
+                console.log('error retrieving refresh token');
+            }
+        });
+
         chrome.storage.sync.set({ access_token: accessToken }, function () {
             console.log('updated access token');
         });
